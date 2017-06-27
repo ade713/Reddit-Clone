7c56e5d7e5554722060e856bbae65fc7c8f20238
Ade <adefarquhar@gmail.com>
2017-06-26 17:01:42 -0700

    redditapp, crazy null bug

---
+++ config/routes.rb
@@ -0,0 +1,23 @@
+Rails.application.routes.draw do
+  get 'posts/new'
+
+  get 'posts/edit'
+
+  get 'posts/show'
+
+  # get 'subs/index'
+  #
+  # get 'subs/new'
+  #
+  # get 'subs/edit'
+  #
+  # get 'subs/show'
+
+  resources :users, only: [:new, :create]
+  resource :session, only: [:new, :create, :destroy]
+
+  resources :subs
+  resources :posts, except: [:index]
+
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+end
